57e142cbaea575aa2f4739dbe02a518596a42f98
cole <cole@DESKTOP-GIV63JT>
2023-09-12 16:20:17 -0600

    Added front-end controllers and views

---
+++ ExpenseTrackerWeb/Controllers/Account.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackerWeb.Controllers
+{
+    public class Account : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ ExpenseTrackerWeb/Controllers/Expenses.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ExpenseTrackerWeb.Controllers
+{
+    public class Expenses : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+    }
+}
